da34e2c4ac653b72de5f733202ab258d7897efc5
José Campillo <jcampillo1207@gmail.com>
2025-03-25 15:46:49 -0600

    update

--- app/(app)/dashboard/[branchId]/products/[productId]/_components/product-stats.tsx
+++ app/(app)/dashboard/[branchId]/products/[productId]/_components/product-stats.tsx
@@ -35,52 +35,55 @@ export const ProductStats = ({
 }) => {
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   return (
-    <div className="w-full h-14 items-stretch gap-x-5 flex flex-wrap">
-      <span className="flex flex-col gap-y-1 items-start justify-start">
-        <p className="text-sm font-semibold">Creado el:</p>
-        <Badge
-          variant={"outline"}
-          className="bg-sidebar hover:bg-sidebar/80 cursor-default text-muted-foreground gap-x-1"
-        >
-          <Circle className="size-1.5 fill-current text-current opacity-50" />
-          {formatInTimeZone(product.created_at, timeZone, "PPP hh:mm aa", {
-            locale: es,
-          })}
-        </Badge>
-      </span>
-      <Separator className="h-full" orientation="vertical" />
-      <span className="flex flex-col gap-y-1 items-start justify-start">
-        <p className="text-sm font-semibold">Última edición:</p>
-        <Badge
-          variant={"outline"}
-          className="bg-sidebar hover:bg-sidebar/80 cursor-default text-muted-foreground gap-x-1"
-        >
-          <Circle className="size-1.5 fill-current text-current opacity-50" />
-          {formatInTimeZone(product.updated_at, timeZone, "PPP hh:mm aa", {
-            locale: es,
-          })}
-        </Badge>
-      </span>
-      <Separator className="h-full" orientation="vertical" />
-      <span className="flex flex-col gap-y-1 items-start justify-start">
-        <p className="text-sm font-semibold">Estado:</p>
-        <Badge
-          variant={"secondary"}
-          className={cn(
-            product.status === "draft" &&
-              "bg-primary/20 text-amber-800 hover:bg-primary/10",
-            product.status === "active" &&
-              "bg-success/20 text-success hover:bg-success/10",
-            product.status === "archived" && "bg-muted text-foreground border",
-            product.status === "requires_verification" &&
-              "bg-background text-muted-foreground border-border hover:bg-background/90 hover:text-muted-foreground",
-            product.status === "deleted" &&
-              "bg-destructive/20 text-destructive hover:bg-destructive/10"
-          )}
-        >
-          {getStatusLabel(product.status)}
-        </Badge>
-      </span>
+    <div className="w-full h-fit items-start justify-start overflow-auto">
+      <div className="w-fit h-14 items-stretch gap-x-5 flex flex-wrap">
+        <span className="flex flex-col gap-y-1 items-start justify-start">
+          <p className="text-sm font-semibold">Creado el:</p>
+          <Badge
+            variant={"outline"}
+            className="bg-sidebar hover:bg-sidebar/80 cursor-default text-muted-foreground gap-x-1"
+          >
+            <Circle className="size-1.5 fill-current text-current opacity-50" />
+            {formatInTimeZone(product.created_at, timeZone, "PPP hh:mm aa", {
+              locale: es,
+            })}
+          </Badge>
+        </span>
+        <Separator className="h-full" orientation="vertical" />
+        <span className="flex flex-col gap-y-1 items-start justify-start">
+          <p className="text-sm font-semibold">Última edición:</p>
+          <Badge
+            variant={"outline"}
+            className="bg-sidebar hover:bg-sidebar/80 cursor-default text-muted-foreground gap-x-1"
+          >
+            <Circle className="size-1.5 fill-current text-current opacity-50" />
+            {formatInTimeZone(product.updated_at, timeZone, "PPP hh:mm aa", {
+              locale: es,
+            })}
+          </Badge>
+        </span>
+        <Separator className="h-full" orientation="vertical" />
+        <span className="flex flex-col gap-y-1 items-start justify-start">
+          <p className="text-sm font-semibold">Estado:</p>
+          <Badge
+            variant={"secondary"}
+            className={cn(
+              product.status === "draft" &&
+                "bg-primary/20 text-amber-800 hover:bg-primary/10",
+              product.status === "active" &&
+                "bg-success/20 text-success hover:bg-success/10",
+              product.status === "archived" &&
+                "bg-muted text-foreground border",
+              product.status === "requires_verification" &&
+                "bg-background text-muted-foreground border-border hover:bg-background/90 hover:text-muted-foreground",
+              product.status === "deleted" &&
+                "bg-destructive/20 text-destructive hover:bg-destructive/10"
+            )}
+          >
+            {getStatusLabel(product.status)}
+          </Badge>
+        </span>
+      </div>
     </div>
   );
 };
