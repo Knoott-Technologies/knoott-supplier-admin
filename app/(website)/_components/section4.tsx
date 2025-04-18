5e3b843b47ab976df63220ff8ebbeef31d592b39
José Campillo <jcampillo1207@gmail.com>
2025-04-17 19:35:31 -0600

    update

--- app/(website)/_components/section4.tsx
+++ app/(website)/_components/section4.tsx
@@ -10,7 +10,7 @@ import { Separator } from "@/components/ui/separator";
 
 export const Section4 = ({ user }: { user: User | null }) => {
   return (
-    <section className="w-full items-start lg:items-center justify-start lg:justify-center flex flex-col px-5 md:px-7 lg:px-14 xl:px-36 2xl:px-56 py-20 pb-0 md:py-28 md:pb-0 lg:py-32 relative bg-sidebar h-[calc(100dvh_-_56px)]">
+    <section className="w-full items-start lg:items-center justify-start lg:justify-center flex flex-col px-5 md:px-7 lg:px-14 xl:px-36 2xl:px-56 py-20 pb-0 md:py-28 md:pb-0 lg:py-32 relative bg-sidebar h-[calc(100vh_-_56px)]">
       <div className="w-full grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-10 lg:gap-14">
         <div className="w-full h-fit items-start justify-start flex flex-col gap-y-3 shrink-0">
           <BlurFade direction="right" inView delay={0}>
